9d620c8a8e380beea5cc8a0f00f82c7a69b9a861
Todd Jordan <todd.jordan@db.com>
2015-03-20 18:13:33 -0400

    code selection of people

--- app/menu/PersonMenuController.js
+++ app/menu/PersonMenuController.js
@@ -3,8 +3,24 @@
 var addressBookApp = angular.module('addressBook.personMenu', []);
 
 addressBookApp.controller('PersonMenuController', ['$scope', 'PersonService', function($scope, personService) {
-  personService.loadPeople();
-  $scope.peopleListObj = personService.getPeopleListObj();
+  var successfulLoad = function() {
+    $scope.peopleListObj = personService.getPeopleListObj();    
+    $scope.selectedPerson = $scope.peopleListObj.peopleList[0].name;
+    console.log("selected person: " + $scope.selectedPerson);
+  }
+  var loadFail = function() {
+    console.log("Nobody loves you");
+  }
+  personService.loadPeople(successfulLoad, loadFail);
+
   $scope.currentSort = 'name';
+  $scope.isActiveMenuItem = function(name) {
+    console.log("name active?: " + name);
+    return name===$scope.selectedPerson;
+  };
+  $scope.selectPerson = function(name) {
+    console.log("name selected: " + name);
+    $scope.selectedPerson = name;
+  };
 }]);
 
--- app/services/PersonService.js
+++ app/services/PersonService.js
@@ -1,22 +1,30 @@
 'use strict';
 
 function PersonService($http) {
+  var nullPerson = {
+    name:""
+  };
   var peopleListObj = {
-    peopleList:[]
+    peopleList:[],
+    getFirst:function() {
+      return this.peopleList.length>0?peopleList[0]:nullPerson;
+    }
   };
 
   return {
     
     
-    loadPeople: function() {
+    loadPeople: function(success, fail) {
       $http.get('/api/people').
         success(function(data, status, headers, config) {
           if(data.people) {
             peopleListObj.peopleList = data.people;
+            success();
           }
         }).
         error(function(data, status, headers, config) {
           peopleListObj.peopleList = [];
+          fail();
         });
     },
 
