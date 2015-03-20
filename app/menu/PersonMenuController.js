352e79587a0acc43b1294da0df2a2b2f06ff487f
Todd Jordan <todd.jordan@db.com>
2015-03-20 03:34:38 -0400

    dump users to left menu

--- app/menu/PersonMenuController.js
+++ app/menu/PersonMenuController.js
@@ -3,6 +3,8 @@
 var addressBookApp = angular.module('addressBook.personMenu', []);
 
 addressBookApp.controller('PersonMenuController', ['$scope', 'PersonService', function($scope, personService) {
-  $scope.text = "hello world";
+  personService.loadPeople();
+  $scope.peopleListObj = personService.getPeopleListObj();
+  $scope.currentSort = 'name';
 }]);
 
